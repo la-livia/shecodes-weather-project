89d34f58abc5d1bbcca050dc7cec495c7478d251
la-livia <114690192+la-livia@users.noreply.github.com>
2022-10-09 17:01:42 +0200

    Added the relevant icon from the API

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
 function formatDate(timestamp) {
   let now = new Date(timestamp);
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  if (date < 10) {
+    date = `0${date}`;
   }
   let days = [
     "Sunday",
@@ -18,7 +29,16 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[now.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${month} ${date}, ${day} ${hours}:${minutes}`;
 }
 
 function showTemperature(response) {
@@ -28,12 +48,22 @@ function showTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector("#icon");
+  //let sunriseElement = document.querySelector("#sunrise");
+  //let sunsetElement = document.querySelector("#sunset");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   timeElement.innerHTML = formatDate(response.data.dt * 1000);
+  //sunriseElement.innerHTML = response.data.dt * 1000;
+  //sunsetElement.innerHTML = response.data.dt * 1000;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[O].description);
 }
 
 let apiKey = "1df0aac02b4f8a54bc1aee5bafade766";
