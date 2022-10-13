a913284f572be822f064f7907b11a6c4352264c1
la-livia <114690192+la-livia@users.noreply.github.com>
2022-10-13 10:35:53 +0200

    Working on forecast API

--- src/script.js
+++ src/script.js
@@ -41,7 +41,8 @@ function formatDate(timestamp) {
   return `${month} ${date}, ${day} ${hours}:${minutes}`;
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -61,7 +62,7 @@ function showForecast() {
               <div class="col-2">
                 <div class="weather-forecast-date">${day}</div>
                 <img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  src="https://openweathermap.org/img/wn/50d@2x.png"
                   alt=""
                   width="42"
                 />
@@ -75,6 +76,12 @@ function showForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "8cac06f7ab6c10287cd06a316ff84a57";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function showTemperature(response) {
@@ -102,7 +109,8 @@ function showTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[O].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -161,4 +169,3 @@ let celsius = document.querySelector("#celsius");
 celsius.addEventListener("click", showCelsius);
 
 search("London");
-showForecast();
